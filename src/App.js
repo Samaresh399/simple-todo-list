e967ee6cfaeef7e15e3f3e07562f2ae306671ea9
Samaresh Adak <77281101+Samaresh399@users.noreply.github.com>
2024-10-10 19:13:37 +0530

    Created add task control and task list section.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-slate-200 flex justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Hello, Tailwind in React!
-      </h1>
+    <div className="grid place-items-center bg-slate-200 h-screen">
+      <div class="md:w-[500px] h-5/6 p-3 flex flex-col rounded shadow-xl bg-white space-y-2">
+        <div class="flex items-center justify-between p-2 gap-2">
+          <input class="w-5/6 p-2 rounded-md border-2 border-slate-200 focus:border-slate-300 shadow-sm focus:shadow-lg focus:outline-none focus:scale-105 transition" />
+          <button class="py-2 px-3 rounded-md border-2 border-green-600 bg-green-500 text-white hover:border-green-500 hover:bg-green-400 focus:border-green-700 focus:bg-green-600">
+            Add
+          </button>
+        </div>
+        <div class="flex-grow bg-slate-100 shadow-inner flex flex-col">
+          <div class="h-full grid place-items-center">
+            <span class="text-xl text-slate-400">No Task Added Yet!</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
